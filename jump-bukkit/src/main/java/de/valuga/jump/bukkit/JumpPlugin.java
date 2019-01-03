f0f1cdf0aa1dc319f069bf88e633d3417ddb9ce9
Nico Didszuweit <nicondlouis@gmail.com>
2019-01-03 04:44:59 +0100

    Register listener

--- jump-bukkit/src/main/java/de/valuga/jump/bukkit/JumpPlugin.java
+++ jump-bukkit/src/main/java/de/valuga/jump/bukkit/JumpPlugin.java
@@ -1,6 +1,8 @@
 package de.valuga.jump.bukkit;
 import de.valuga.jump.DefaultJumpAndRunOperator;
 import de.valuga.jump.JumpAndRuns;
+import de.valuga.jump.bukkit.listener.JumpAndRunListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -11,5 +13,6 @@ public class JumpPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         JumpAndRuns.setOperator(new DefaultJumpAndRunOperator());
+        Bukkit.getPluginManager().registerEvents(new JumpAndRunListener(), this);
     }
 }
