3ba4ea791607967a546dd4cff0f9963b72847331
Nico Didszuweit <nicondlouis@gmail.com>
2019-01-03 03:55:13 +0100

    Add default jump and run operator and edit jump and run logic

--- jump-config/src/main/java/de/valuga/jump/DefaultJumpAndRunOperator.java
+++ jump-config/src/main/java/de/valuga/jump/DefaultJumpAndRunOperator.java
@@ -1,6 +1,66 @@
 package de.valuga.jump;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.bukkit.entity.Player;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.*;
+
 /**
  * @author Nico_ND1
+ * @since 0.0.2
  */
 public class DefaultJumpAndRunOperator implements JumpAndRunOperator {
+
+    private final Gson gson = new GsonBuilder()
+        .excludeFieldsWithoutExposeAnnotation()
+        .setPrettyPrinting()
+        .serializeNulls()
+        .registerTypeAdapter(JumpAndRun.class, new JumpAndRun())
+        .create();
+    private final File configDirectory = new File("jump-and-runs");
+    private final Map<UUID, JumpAndRunSession> sessions = Collections.synchronizedMap(new HashMap<>());
+
+    @Override
+    public void createJumpAndRun(JumpAndRun jumpAndRun) {
+        if (!this.configDirectory.exists()) this.configDirectory.mkdir();
+
+        try (FileWriter fileWriter = new FileWriter(new File(this.configDirectory, jumpAndRun.getName() + ".json"))) {
+            fileWriter.write(this.gson.toJson(jumpAndRun, JumpAndRun.class));
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<JumpAndRun> getJumpAndRuns() {
+        final List<JumpAndRun> jumpAndRuns = new ArrayList<>();
+
+        for (File file : Objects.requireNonNull(this.configDirectory.listFiles())) {
+            try {
+                jumpAndRuns.add(this.gson.fromJson(new InputStreamReader(new FileInputStream(file), Charset.forName("UTF-8")), JumpAndRun.class));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return jumpAndRuns;
+    }
+
+    @Override
+    public JumpAndRunSession startJumpAndRun(Player player, JumpAndRun jumpAndRun) {
+        return null;
+    }
+
+    @Override
+    public JumpAndRunSession getJumpSessionInfo(Player player) {
+        return this.sessions.get(player.getUniqueId());
+    }
+
+    @Override
+    public void finishJumpAndRun(JumpAndRunSession jumpAndRunSession) {
+
+    }
 }
--- jump-parent/src/main/java/de/valuga/jump/JumpAndRun.java
+++ jump-parent/src/main/java/de/valuga/jump/JumpAndRun.java
@@ -1,19 +1,43 @@
 package de.valuga.jump;
 
+import com.google.gson.*;
+import com.google.gson.annotations.Expose;
 import de.valuga.jump.reward.JumpAndRunReward;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * @author Nico_ND1
  */
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 @Data
-public abstract class JumpAndRun {
+public class JumpAndRun implements JsonDeserializer<JumpAndRun> {
 
-    private final SerializableLocation spawnLocation;
-    private final SerializableLocation[] checkpoints;
-    private final String name;
-    private final JumpAndRunReward reward;
+    @Expose private final SerializableLocation spawnLocation;
+    @Expose private final List<SerializableLocation> checkpoints;
+    @Expose private final String name;
+    @Expose private final JumpAndRunReward reward;
+    @Expose private final int deathAt;
 
+    @Override
+    public JumpAndRun deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        final JsonObject jsonObject = json.getAsJsonObject();
+        final SerializableLocation spawnLocation = context.deserialize(jsonObject.get("spawnLocation"), SerializableLocation.class);
+        final List<SerializableLocation> checkpoints = StreamSupport.stream(jsonObject.getAsJsonArray("checkpoints").spliterator(), false)
+            .map((Function<JsonElement, SerializableLocation>) jsonElement -> context.deserialize(jsonElement, SerializableLocation.class))
+            .collect(Collectors.toList());
+        final String name = jsonObject.get("name").getAsString();
+        final JumpAndRunReward reward = null; // TODO: Implement
+        final int deathAt = jsonObject.get("deathAt").getAsInt();
+
+        return new JumpAndRun(spawnLocation, checkpoints, name, reward, deathAt);
+    }
 }
