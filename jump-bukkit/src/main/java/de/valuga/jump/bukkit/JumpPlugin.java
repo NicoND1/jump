40ec6bd8048e6ca265a253c3855c255a1b7af3a4
Nico Didszuweit <nicondlouis@gmail.com>
2019-01-03 04:11:02 +0100

    Add bukkit hook module

---
+++ jump-bukkit/src/main/java/de/valuga/jump/bukkit/JumpPlugin.java
@@ -0,0 +1,13 @@
+package de.valuga.jump.bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Nico_ND1
+ */
+public class JumpPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
