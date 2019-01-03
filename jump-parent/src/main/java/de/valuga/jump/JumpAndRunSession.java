5be949a6ff3fb9680f36da58154f868ae031e06f
Nico Didszuweit <nicondlouis@gmail.com>
2019-01-03 18:10:12 +0100

    Apply player's head rotation on reset

--- jump-parent/src/main/java/de/valuga/jump/JumpAndRunSession.java
+++ jump-parent/src/main/java/de/valuga/jump/JumpAndRunSession.java
@@ -1,6 +1,7 @@
 package de.valuga.jump;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 /**
@@ -13,19 +14,25 @@ public class JumpAndRunSession {
     private final Player player;
     private final long startTime = System.currentTimeMillis();
     private final JumpAndRun jumpAndRun;
-    private int checkpoint = 0;
+    private int checkpoint = -1;
 
     public void reset() {
-        if (this.checkpoint == 0)
-            this.player.teleport(this.jumpAndRun.getSpawnLocation().toLocation());
+        if (this.checkpoint == -1)
+            this.player.teleport(this.applyHeadRotation(this.jumpAndRun.getSpawnLocation().toLocation().clone()));
         else
-            this.player.teleport(this.jumpAndRun.getCheckpoint(this.checkpoint).toLocation());
+            this.player.teleport(this.applyHeadRotation(this.jumpAndRun.getCheckpoint(this.checkpoint).toLocation().clone().add(0.5, 1, 0.5)));
+    }
+
+    private Location applyHeadRotation(Location location) {
+        location.setYaw(this.player.getLocation().getYaw());
+        location.setPitch(this.player.getLocation().getPitch());
+        return location;
     }
 
     public void achieveCheckpoint(int checkpoint) {
         if (this.checkpoint == checkpoint) return;
 
-        if(checkpoint == this.jumpAndRun.getCheckpoints().size() - 1) {
+        if (checkpoint == this.jumpAndRun.getCheckpoints().size() - 1) {
             // Finish because the last checkpoint is the goal
             JumpAndRuns.getOperator().finishJumpAndRun(this);
             return;
