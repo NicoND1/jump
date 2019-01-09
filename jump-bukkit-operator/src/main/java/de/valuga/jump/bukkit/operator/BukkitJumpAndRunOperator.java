6c1aefc717bbb7ac7739b2f96cc3de29305e8445
Nico Didszuweit <nicondlouis@gmail.com>
2019-01-09 16:51:35 +0100

    Add stats and more

--- jump-bukkit-operator/src/main/java/de/valuga/jump/bukkit/operator/BukkitJumpAndRunOperator.java
+++ jump-bukkit-operator/src/main/java/de/valuga/jump/bukkit/operator/BukkitJumpAndRunOperator.java
@@ -3,6 +3,7 @@ import de.valuga.jump.JsonJumpAndRunOperator;
 import de.valuga.jump.JumpAndRun;
 import de.valuga.jump.JumpAndRunSession;
 import de.valuga.jump.hotbar.HotbarManager;
+import de.valuga.jump.stats.HighscoreStatistic;
 import org.bukkit.entity.Player;
 
 import java.util.Optional;
@@ -12,6 +13,8 @@ import java.util.Optional;
  * @since 0.0.1
  */
 public class BukkitJumpAndRunOperator extends JsonJumpAndRunOperator {
+
+    private final HighscoreStatistic highscoreStatistic = new HighscoreStatistic("highscores");
 
     @Override
     public JumpAndRunSession startJumpAndRun(Player player, JumpAndRun jumpAndRun) {
@@ -32,5 +35,19 @@ public class BukkitJumpAndRunOperator extends JsonJumpAndRunOperator {
         this.sessions.remove(jumpAndRunSession.getPlayer().getUniqueId());
         jumpAndRunSession.getPlayer().sendMessage("Finish");
         HotbarManager.getInstance().setInventory(jumpAndRunSession.getPlayer(), "empty");
+
+        final long diff = System.currentTimeMillis() - jumpAndRunSession.getStartTime();
+        this.highscoreStatistic.getPlayerStats(jumpAndRunSession.getPlayer().getUniqueId(), jumpAndRunSession.getJumpAndRun().getName()).thenAccept(playerStats -> {
+            if (playerStats.getHighscore() > diff) {
+                this.highscoreStatistic.trackPlayer(
+                    jumpAndRunSession.getPlayer().getUniqueId(),
+                    jumpAndRunSession.getJumpAndRun().getName(),
+                    diff,
+                    System.currentTimeMillis(),
+                    () -> {
+                    }
+                );
+            }
+        });
     }
 }
--- jump-stats/src/main/java/de/valuga/jump/stats/HighscoreStatistic.java
+++ jump-stats/src/main/java/de/valuga/jump/stats/HighscoreStatistic.java
@@ -4,16 +4,25 @@ import de.valuga.database.factory.DatabaseFactory;
 import de.valuga.database.table.DataItem;
 import de.valuga.database.table.DataItem.Type;
 import de.valuga.database.table.Table;
+import de.valuga.database.table.WhereStatement;
 import de.valuga.database.table.executor.Upsert;
 import de.valuga.database.table.executor.Value;
+import de.valuga.database.table.query.AdditionalQueryData;
+import de.valuga.database.table.query.Query;
+import de.valuga.database.table.query.Result;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.sql.SQLException;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author Nico_ND1
  */
 public class HighscoreStatistic {
+
+    // TODO: Make jump-stats module more abstract so it can be used for more porpuses
 
     private final Table table;
 
@@ -22,12 +31,12 @@ public class HighscoreStatistic {
         final DatabaseFactory databaseFactory = databaseConnection.getDatabaseFactory();
         this.table = databaseFactory.createTable(tableName, new DataItem[]{
             databaseFactory.createDataItem("uuid", Type.UUID, true, false, true, 0),
-            databaseFactory.createDataItem("name", Type.TEXT, true, false, true, 16),
+            databaseFactory.createDataItem("name", Type.TEXT, false, false, true, 0),
             databaseFactory.createDataItem("highscore", Type.BIGINT, false, false, true, 0),
             databaseFactory.createDataItem("time", Type.BIGINT, false, false, true, 0)
         });
         try {
-            databaseConnection.getConnection().createStatement().executeUpdate(table.createTable());
+            databaseConnection.getConnection().createStatement().executeUpdate(this.table.createTable());
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -43,6 +52,44 @@ public class HighscoreStatistic {
             databaseFactory.createValue("time", time)
         });
         databaseConnection.execute(runnable, upsert);
+    }
+
+    public CompletableFuture<PlayerStats> getPlayerStats(UUID uuid, String name) {
+        final DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+        final DatabaseFactory databaseFactory = databaseConnection.getDatabaseFactory();
+        final Query query = databaseFactory.createQuery(this.table, this.table.getDataItems(), new WhereStatement[]{
+            databaseFactory.createWhereStatement("uuid", uuid),
+            databaseFactory.createWhereStatement("name", name)
+        }, new AdditionalQueryData[0]);
+        final CompletableFuture<PlayerStats> completableFuture = new CompletableFuture<>();
+        databaseConnection.execute(query).thenAccept(results -> completableFuture.complete(new PlayerStats(results)));
+
+        return completableFuture;
+    }
+
+    @AllArgsConstructor
+    @Data
+    public static class PlayerStats {
+
+        private UUID uuid;
+        private String name;
+        private long highscore = Long.MAX_VALUE;
+        private long time;
+
+        public PlayerStats(Result[] results) {
+            for (Result result : results) {
+                if (result == null) continue;
+                if (result.getDataItem().getName().equalsIgnoreCase("uuid"))
+                    this.uuid = UUID.fromString(result.getResult().toString());
+                else if (result.getDataItem().getName().equalsIgnoreCase("name"))
+                    this.name = result.getResult().toString();
+                else if (result.getDataItem().getName().equalsIgnoreCase("highscore"))
+                    this.highscore = Long.valueOf(result.getResult().toString());
+                else if (result.getDataItem().getName().equalsIgnoreCase("time"))
+                    this.time = Long.valueOf(result.getResult().toString());
+            }
+        }
+
     }
 
 }
